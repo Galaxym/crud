0f821387832bef2da77a8c33c34eb6415d6a23f6
Galaxym <simon.uldry@formation.orif.ch>
2015-10-01 14:22:39 +0200

    home.php changed

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,1 +1,2 @@
-<h2 class="name"><a href="index.php/stock/items_list">Consulter le stock</a></h2>
+<h2 class="name"><a href="index.php/stock/items_list">Consulter le stock d'item</a></h2>
+<span>Des modifications ont été sauvegardées et envoyées sur GitHub</span>
